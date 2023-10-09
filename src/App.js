76bcf4c5c874e56e7afa31d0bf31723a86617e81
dichonw <dichowicaksono10@gmail.com>
2023-10-09 18:30:14 +0700

    update exchange rate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Table from 'react-bootstrap/table';
 
 function App() {
+  const API_KEY = 'beee3e3f2df5447bb5c45eaca885201e';
+  const symbols = 'CAD, EUR, IDR, JPY, CHF, GBP';
+
+  const [data, setData] = useState([]);
+  
+  useEffect(() => {
+    axios
+    .get(`https://api.currencyfreaks.com/v2.0/rates/latest?apikey=${API_KEY}&symbols=${symbols}`)
+      .then((response) => {
+        const currency = response.data.rates;
+        const currencyData = Object.keys(currency).map((rate) => ({
+          currencyName: rate,
+          weBuy: Number(currency[rate] * 1.05).toFixed(4),
+          exchangeRate: Number(currency[rate] * 1.0).toFixed(4),
+          weSell: Number(currency[rate] * 0.95).toFixed(4),
+        }));
+        setData(currencyData);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="container">
+        <h1 className="text-light my-3">Currency Rates</h1>
+        <Table striped bordered hover variant="dark">
+          <thead>
+            <tr>
+              <td className="fw-bold">Currency</td>
+              <td className="fw-bold">We Buy</td>
+              <td className="fw-bold">Exchange Rate</td>
+              <td className="fw-bold">We Shell</td>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((items, index) => (
+            <tr key={index}>
+              <td>{items.currencyName.toString()}</td>
+              <td>{items.weBuy.toString()}</td>
+              <td>{items.exchangeRate.toString()}</td>
+              <td>{items.weSell.toString()}</td>
+            </tr>
+            ))}
+          </tbody>
+        </Table>
+        <p className="text-light">
+          Rates are based from 1 USD <br/>
+          This application uses API from <a href="https://currencyfreaks.com">https://currencyfreaks.com</a> 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
